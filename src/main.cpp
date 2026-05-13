eb6e024b7ab5b87cde97896b441ef5c4365e73bf
Tim O'Brien <timo@t413.com>
2026-05-13 16:24:35 -0700

    initial basic pio project

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <M5Unified.h>
+
+void setup() {
+    auto cfg = M5.config();
+    M5.begin(cfg);
+    Serial.begin(115200);
+}
+
+void loop() {
+    M5.update();
+}
---
+++ test/test_basics.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+using namespace std;
+
+TEST(FirstTest, Basic) {
+    EXPECT_EQ(1, 2 - 1);
+}
